e93f59977b5bd9d9b4aec40387157a9bb3a939a0
nour <nour.soubat@gmail.com>
2021-01-06 17:57:23 +0100

    adding website

---
+++ config.py
@@ -0,0 +1,3 @@
+
+class Config(object):
+    SECRET_KEY = 'U\x08\x90\xd5\xf3;o\xb6\x9c9$\x81'
---
+++ website.py
@@ -0,0 +1,4 @@
+from app import app
+
+
+app.run(debug=True, host="2a01:e0a:2d9:dca0:b4ff:7be8:dbd5:d684")
